de6b2ca96e655450fc38c2660a118931f556374c
FreddyASierraJ <tobitage@gmail.com>
2024-09-09 20:44:22 -0500

    add test methods, add, substract, mutliply, divide

--- CalculatorTests.cs
+++ CalculatorTests.cs
@@ -13,5 +13,51 @@ namespace LearnMyCalculatorApp.tests
             Assert.IsNull(calculator);
             Assert.IsTrue(false);
         }
+        [TestMethod]
+        public void AddTest() 
+        {
+            // Arrange
+            var calculator = new Calculator();
+            
+            //Act
+            var actual = calculator.Add(1, 1);
+            
+            //Assert
+            Assert.AreEqual(2, actual);
+        }
+        [TestMethod]
+        public void SubtractTest()
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var actual= calculator.Subtract(3, 1);
+
+            //Assert
+            Assert.AreEqual(2, actual);
+        }
+        [TestMethod]
+        public void MultiplyTest() 
+        {
+            //Arrange
+            var calculator = new Calculator();
+
+            //Act
+            var actual =calculator.Multiply(3, 1);
+
+            //Assert
+            Assert.AreEqual(3, actual);
+        }
+        [TestMethod]
+        public void DivideTest() 
+        {
+            var calculator = new Calculator();
+
+            var actual=calculator.Divide(3, 0);
+
+            Assert.ThrowsException<InvalidOperationException>(() => actual);
+        }
+
     }
 }
